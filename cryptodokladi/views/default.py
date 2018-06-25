d89a65c426a20b3a0f94ed0aa479e2e054b110ad
Alko89 <alefer89@gmail.com>
2018-06-25 20:13:15 +0200

    remove menus

--- cryptodokladi/views/default.py
+++ cryptodokladi/views/default.py
@@ -16,10 +16,10 @@ from ..models import Page, User
 # regular expression used to find WikiWords
 wikiwords = re.compile(r"\b([A-Z]\w+[A-Z]+\w+)")
 
-@view_config(route_name='view_wiki')
-def view_wiki(request):
-    next_url = request.route_url('view_page', pagename='FrontPage')
-    return HTTPFound(location=next_url)
+# @view_config(route_name='view_wiki')
+# def view_wiki(request):
+#     next_url = request.route_url('view_page', pagename='FrontPage')
+#     return HTTPFound(location=next_url)
 
 @view_config(route_name='view_page', renderer='../templates/view.jinja2', permission='view')
 def view_page(request):
@@ -69,9 +69,3 @@ def add_page(request):
         return HTTPFound(location=next_url)
     save_url = request.route_url('add_page', pagename=pagename)
     return dict(pagename=pagename, pagetitle='', pagesubtitle='', pagedata='', save_url=save_url)
-
-@view_config(route_name='solidity', renderer='../templates/solidity.jinja2')
-def solidity(request):
-    user = request.user
-    
-    return dict(user=user)
