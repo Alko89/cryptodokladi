52a6f9a6f434e2ec37abc26eb2e692c5ce62188a
Alko89 <alefer89@gmail.com>
2018-08-18 12:50:19 +0200

    Revert "remove menus"
    This reverts commit d89a65c426a20b3a0f94ed0aa479e2e054b110ad.

--- cryptodokladi/routes.py
+++ cryptodokladi/routes.py
@@ -11,7 +11,9 @@ from .models import Page, User
 
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('login', '/')
+    config.add_route('view_wiki', '/')
+    config.add_route('solidity', '/solidity')
+    config.add_route('login', '/login')
     config.add_route('logout', '/logout')
 
     config.add_route('songs_list', 'song')
